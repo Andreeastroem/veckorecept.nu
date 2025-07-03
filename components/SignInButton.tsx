ad9aa12258f78a157fc10b076f9f764a912de900
Andree Åström <andre.astrom@adlibris.com>
2025-07-03 21:09:40 +0200

    fix: change signin from router to next link

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,16 +1,12 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function SignInButton() {
-  const router = useRouter();
-
   return (
-    <button
+    <Link
       className="backdrop-blur-xl bg-gradient-to-r from-primary/20 to-accent/20 hover:from-primary/30 hover:to-accent/30 text-primary-foreground rounded-xl px-6 py-3 border border-border shadow-lg shadow-ring/20 transition-all duration-300 hover:shadow-xl hover:shadow-ring/30 hover:scale-105 font-medium"
-      onClick={() => router.push("/signin")}
+      href="/signin"
     >
       Sign in
-    </button>
+    </Link>
   );
 }
