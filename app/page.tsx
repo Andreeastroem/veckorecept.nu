5744876a07961a30a3866d373deeb82a0c435644
André Åström <andreeastroem@gmail.com>
2025-09-11 23:07:28 +0200

    fix: consistent layouting

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,19 @@
-import SignOutButton from "@/components/SignOutButton";
 import RecipeList from "./components/RecipeList";
 import AddRecipe from "./components/AddRecipe/AddRecipe";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { MainLayout, HeaderLayout } from "./components/PageLayout";
 
 export default function Home() {
   return (
     <>
-      <header className="max-w-[640px] mx-auto sticky top-0 z-10 backdrop-blur-xl bg-background/20 border-b border-border p-4 flex flex-row justify-between items-center rounded-b-2xl shadow-2xl shadow-ring/20">
-        <h1 className="text-2xl font-bold text-primary">Veckorecept.nu</h1>
-        <SignOutButton />
-      </header>
-      <main className="p-8 flex flex-col gap-8 max-w-[640px] mx-auto">
+      <HeaderLayout />
+      <MainLayout>
         <div className="text-center space-y-4">
           <h1 className="text-6xl font-bold text-primary">Veckorecept.nu</h1>
           <p className="text-muted-foreground text-lg">En vecka i taget</p>
         </div>
         <Content />
-      </main>
+      </MainLayout>
     </>
   );
 }
