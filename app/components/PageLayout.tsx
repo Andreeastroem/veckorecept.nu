918bb0eb9aba9cd02e09348639e194656c1fa883
André Åström <andreeastroem@gmail.com>
2025-09-12 13:26:16 +0200

    fix: change to standard login from convex docs

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function HeaderLayout() {
   return (
-    <header className="max-w-[640px] mx-auto sticky top-0 z-10 backdrop-blur-xl bg-background/20 border-b border-border p-4 flex flex-row justify-between items-center rounded-b-2xl shadow-2xl shadow-ring/20">
+    <header className="mx-4 max-w-[640px] md:mx-auto sticky top-0 z-10 backdrop-blur-xl bg-background/20 border-b border-border p-4 flex flex-row justify-between items-center rounded-b-2xl shadow-2xl shadow-ring/20">
       <Link href="/" className="hover:underline">
         <h1 className="text-2xl font-bold text-primary">Veckorecept.nu</h1>
       </Link>
