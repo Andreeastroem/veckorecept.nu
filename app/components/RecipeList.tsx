c8b98568d45b49ee507aac77acc78d75b244429d
André Åström <andreeastroem@gmail.com>
2025-11-03 00:07:33 +0100

    feat: add tooltips

--- app/components/RecipeList.tsx
+++ app/components/RecipeList.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import Link from "next/link";
@@ -15,19 +20,65 @@ export default function RecipeList() {
       <div className="text-accent-foreground grid gap-1.5">
         {recipes.length > 0 ? (
           recipes.map((recipe) => (
-            <Link
-              className={`${recipe.isCrawled ? "" : "bg-muted pointer-events-none"} flex justify-between w-full backdrop-blur-sm bg-accent border border-border rounded-xl p-4 hover:bg-accent/90 transition-colors duration-300`}
-              href={`/recipe/${recipe.slug}`}
+            <div
               key={recipe._id}
+              className="flex justify-between w-full backdrop-blur-sm bg-accent border border-border rounded-xl p-4 hover:bg-accent/90 transition-colors duration-300"
             >
-              {recipe.name}
-              {recipe.isCrawled ? null : <span>🛠</span>}
-            </Link>
+              <Link
+                className={`${recipe.isCrawled ? "" : "bg-muted pointer-events-none"}`}
+                href={`/recipe/${recipe.slug}`}
+              >
+                {recipe.name}
+              </Link>
+              <StatusSymbol
+                isCrawled={recipe.isCrawled}
+                retries={recipe.retries}
+              />
+            </div>
           ))
         ) : (
           <p>No recipes yet. Add your first recipe to get started!</p>
         )}
       </div>
     </div>
+  );
+}
+
+function StatusSymbol({
+  isCrawled,
+  retries,
+}: {
+  isCrawled: boolean;
+  retries: number;
+}) {
+  if (retries >= 5) {
+    return <FailedToCrawl />;
+  }
+  if (!isCrawled) {
+    return <NotYetCrawled />;
+  }
+
+  return null;
+}
+
+function FailedToCrawl() {
+  return (
+    <Tooltip disableHoverableContent>
+      <TooltipTrigger>❌</TooltipTrigger>
+      <TooltipContent hideArrow className="bg-gray-50">
+        Could not crawl the link - this might be fixed in later versions
+      </TooltipContent>
+    </Tooltip>
+  );
+}
+
+function NotYetCrawled() {
+  return (
+    <Tooltip disableHoverableContent>
+      <TooltipTrigger>🛠</TooltipTrigger>
+      <TooltipContent hideArrow className="bg-gray-50">
+        Recipe is not yet crawled
+      </TooltipContent>
+    </Tooltip>
   );
 }
